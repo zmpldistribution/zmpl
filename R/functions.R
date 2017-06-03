d54233cddbb49e011f28f6d1946521d48ea3652b
Manoel Ferreira dos Santos Neto <santosneto@MacBook-Pro-de-Manoel.local>
2017-06-03 14:34:59 -0300

    4 Commit

--- R/functions.R
+++ R/functions.R
@@ -518,21 +518,103 @@ return(summary(vp))
 #'
 #'@export
 
-descriptive_zmpl(x) <- function(x)
+descriptive_zmpl <- function(x)
 {
 
   barx <- mean(x)
   varx <- var(x)
   skwx <- skewness(x)
-  kurx <- kurtisis(x)
+  kurx <- kurtosis(x)
   Findex <- var(x)/mean(x)
   minx <- min(x)
   maxx <- max(x)
 
-  namescol <- c("Min","Max","Mean","Var","Skew","Kurt","FI")
-  valuescol <- cbind(minx,maxx,barx,varx,skwx,kurx,Findex)
-  out <- rbind(namescol,valuescol)
-  rownames(out) <- c("Measures","Values")
+
+  out <- round(cbind(minx,maxx,barx,varx,skwx,kurx,Findex),2)
+  colnames(out) <- c("Min","Max","Mean","Var","Skew","Kurt","FI")
+  rownames(out) <- c("Values")
 
   out
+}
+
+#'Zero Modified Poisson-Lindley (ZMPL) distribution
+#'
+#'@description The fuction \code{ZMPL()} defines the ZMPL distribution, a two paramenter
+#'distribution. The zero modified Poisson-Lindley distribution is similar
+#'to the Poisson-Lindley distribution but allows zeros as y values. The extra parameter
+#'models the probabilities at zero. The functions dZMPL, pZMPL, qZMPL and rZMPL define
+#'the density, distribution function, quantile function and random generation for
+#'the zero modified Poisson-Lindley distribution.
+#'plotZMPL can be used to plot the distribution. meanZMPL calculates the expected
+#'value of the response for a fitted model.
+#'
+#'@usage
+#'dZMPL(x, mu = 1, sigma = 1, nu=0.1, log = FALSE)
+#'pZMPL(q, mu = 1, sigma = 1,  nu=0.1, lower.tail = TRUE, log.p = FALSE)
+#'qZMPL(p, mu = 1, sigma = 1,  nu=0.1, lower.tail = TRUE, log.p = FALSE)
+#'rZMPL(n, mu = 1, sigma = 1)
+#'plotZMPL(mu = .5, sigma = 1,  nu=0.1, from = 0, to = 0.999, n = 101, ...)
+#'meanZMPL(obj)
+#'
+#' @param x,q vector of observations/quantiles
+#' @param mu vector of scale parameter values
+#' @param sigma vector of shape parameter values
+#' @param log, log.p logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x]
+#' @param p vector of probabilities.
+#' @param n number of observations. If \code{length(n) > 1}, the length is taken to be the number required.
+#' @param from where to start plotting the distribution from
+#' @param to up to where to plot the distribution
+#' @param ... other graphical parameters for plotting
+#'
+#'
+#' @details  The probability massa function of the is given by
+#'
+#'\deqn{ f_{Y}(y;\mu,\delta,p) =\frac{[1-p]\sqrt{\delta+1}}{4\,y^{3/2}\,\sqrt{\pi\mu}}\left[y+\frac{\delta\mu}{\delta+1} \right]\exp\left(-\frac{\delta}{4}\left[\frac{y[\delta+1]}{\delta\mu}+\frac{\delta\mu}{y[\delta+1]}-2\right]\right) I_{(0, \infty)}(y)+  pI_{\{0\}}(y).}
+#'
+#'@return returns a object of the ZMPL distribution.
+#'
+#'@note For the function ZMPL(), mu is the mean and sigma is the precision parameter and nu is the proportion of zeros of the zero adjusted Birnbaum-Saunders distribution.
+#'
+#'@references
+#'Leiva, V., Santos-Neto, M., Cysneiros, F.J.A., Barros, M. (2015) A methodology for stochastic inventory models based on a zero-adjusted Birnbaum-Saunders distribution.
+#'\emph{Applied Stochastic Models in Business and Industry.} \email{10.1002/asmb.2124}.
+#'
+#'@author
+#'Manoel Santos-Neto \email{manoel.ferreira@ufcg.edu.br}, F.J.A. Cysneiros \email{cysneiros@de.ufpe.br}, Victor Leiva \email{victorleivasanchez@gmail.com} and Michelli Barros \email{michelli.karinne@gmail.com}
+#'
+#'@examples FIZMPL()
+#'theta <- seq(0.05,1,l=100)
+#'aux0 <- - ((theta^2)*(theta+2)/( (theta^2) +3*theta+1) )
+#'p0 <- seq(0,1,l=100)
+#'g <- expand.grid(x = theta, y = p0)
+#'z <- FIZMPL(theta,p0)
+#'z[z<0] <- 0
+#'g$z <- z
+#'print(wireframe(z ~ x * y  ,g, xlab=expression(theta), scales = list(arrows = FALSE), ylab=expression(pi), zlab = "FI(X)") )
+#'
+#'
+#'theta <- seq(0.05,1,l=100)
+#'aux0 <- - ((theta^2)*(theta+2)/( (theta^2) +3*theta+1) )
+#'p0 <- seq(min(aux0),0,l=100)
+#'g <- expand.grid(x = theta, y = p0)
+#'z <- FIZMPL(theta,p0)
+#'z[z<0] <- 0
+#'g$z <- z
+#'print(wireframe(z ~ x * y  ,g, xlab=expression(theta), scales = list(arrows = FALSE), ylab=expression(pi), zlab = "FI(X)") )
+#'@export
+
+FIZMPL <- function(theta, p0)
+{
+
+  p <- - (   (theta^2)*(theta+2)/( (theta^2) +3*theta+1) )
+
+
+  mPL <- (theta+2)/(theta*(theta+1))
+  varPL <- ((theta^3) + 4*(theta^2) + 6*theta +2)/(theta*theta*(theta+1)*(theta+1))
+  FIY <- varPL/mPL
+
+  FIX <- mPL*p0 + FIY
+
+  FIX
 }
